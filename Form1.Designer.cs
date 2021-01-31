70dc35b9e7e0b857485b96bd345d99df28a3bd88
muhammad Lattouf <muhammadlattouf@gmail.com>
2021-01-31 18:37:54 +0200

    This is the first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Random_txt_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonFond = new System.Windows.Forms.Button();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.buttonDelet = new System.Windows.Forms.Button();
+            this.richTextBoxLength = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonFond
+            // 
+            this.buttonFond.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonFond.Location = new System.Drawing.Point(12, 147);
+            this.buttonFond.Name = "buttonFond";
+            this.buttonFond.Size = new System.Drawing.Size(358, 45);
+            this.buttonFond.TabIndex = 0;
+            this.buttonFond.Text = "أوجد";
+            this.buttonFond.UseVisualStyleBackColor = true;
+            this.buttonFond.Click += new System.EventHandler(this.buttonFond_Click);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Font = new System.Drawing.Font("Times New Roman", 17.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPassword.Location = new System.Drawing.Point(12, 12);
+            this.textBoxPassword.Multiline = true;
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.ReadOnly = true;
+            this.textBoxPassword.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxPassword.Size = new System.Drawing.Size(358, 128);
+            this.textBoxPassword.TabIndex = 1;
+            // 
+            // buttonDelet
+            // 
+            this.buttonDelet.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDelet.Location = new System.Drawing.Point(376, 147);
+            this.buttonDelet.Name = "buttonDelet";
+            this.buttonDelet.Size = new System.Drawing.Size(74, 45);
+            this.buttonDelet.TabIndex = 3;
+            this.buttonDelet.Text = "حذف";
+            this.buttonDelet.UseVisualStyleBackColor = true;
+            this.buttonDelet.Click += new System.EventHandler(this.buttonDelet_Click);
+            // 
+            // richTextBoxLength
+            // 
+            this.richTextBoxLength.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxLength.Location = new System.Drawing.Point(376, 60);
+            this.richTextBoxLength.Name = "richTextBoxLength";
+            this.richTextBoxLength.Size = new System.Drawing.Size(74, 81);
+            this.richTextBoxLength.TabIndex = 4;
+            this.richTextBoxLength.Text = "";
+            this.richTextBoxLength.KeyDown += new System.Windows.Forms.KeyEventHandler(this.richTextBoxLength_KeyDown);
+            this.richTextBoxLength.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBoxLength_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(379, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 38);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ادخل طول\r\n   الكلمة";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(462, 204);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBoxLength);
+            this.Controls.Add(this.buttonDelet);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.buttonFond);
+            this.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Random Password";
+            this.Load += new System.EventHandler(this.RandomPassword_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFond;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button buttonDelet;
+        private System.Windows.Forms.RichTextBox richTextBoxLength;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿/*                                               بسم الله الرحمن الرحيم
+     هذا البرنامج البسيط يقوم بإيجاد أرقام واحرف ورموز عشوائية بالاستعانة بـ
+     Random Class
+     ويمكن استخدامه لتوليد كلمات مرور مختلفة في كل مرة
+     وقد تم الأخذ بعين الإعتبار إمكانية استخدام جميع الرموز المسموح استخدامها في كلمات المرور بالمواقع العالمية
+*/
+using System; 
+using System.Collections.Generic;  
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;  
+using System.Linq; 
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Random_txt_1
+{
+    public partial class Form1 : Form
+    {
+        int num; // لتحديد عدد خانات كلمة المرور
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void mess()  // تابع يتم وضعه في حال عدم تحقق أحد الشروط المطلوبة
+        {
+            MessageBox.Show("بلا جنون يعني معقول تحط كلمة المرور أطول من 500 خانة!!! 40 خانة بتكفيك وزيادة ",
+                            "!!! هيك تخنتها", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            num = 40; // تم وضع 40 تلقائياً بسبب طلب المستخدم وضع رقم فيه أكثر من 500 خانة
+            richTextBoxLength.Clear();
+            richTextBoxLength.Focus();
+        }
+        private void buttonFond_Click(object sender, EventArgs e)
+        {
+            textBoxPassword.Clear();
+            richTextBoxLength.Focus();
+
+            if (richTextBoxLength.Text.Length != 0) // شرط لمعرفة أن الطول غير خالي من القيم
+            {
+                if(Convert.ToInt32(richTextBoxLength.Text) < 10) // حتى لا تكون كلمة المرور أقل من 10 خانات
+                {
+                    MessageBox.Show("بلا جنون يعني معقول تحط كلمة المرور أقصر من 10 خانات!!! بسمحلك بـ10 خانات على أقل تقدير ",
+                            "!!! هيك تخنتها", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    num = 10; // تم وضع 10 تلقائياً بسبب طلب المستخدم وضع رقم فيه أقل من 10 خانات
+                    richTextBoxLength.Clear();
+                    richTextBoxLength.Focus();
+                }
+                else if (richTextBoxLength.Text.Length >= 4 || Convert.ToInt32(richTextBoxLength.Text) > 500) // شرط لمعرفة أن الطول لا يتجاوز الأربع خانات أو الرقم 500
+                {
+                    mess();
+                }
+                else
+                    num = Convert.ToInt32(richTextBoxLength.Text); // يأخذ المتغير عدد الخانات من المستخدم
+            }
+            else num = 20; // يتم أخذ رقم 20 خانة تلقائياً في حالة لم يضع المستخدم رقم خانات معين
+
+            Random rand = new Random(); // تم التعريف عن منغير من نوع (Random) عشوائي
+            string[] ranstr = { "A", "B", "C", "D", "E", "F","G", "H", "I", "J", "K", "L", // تم وضع الاحرف والأرقام والرموز
+            "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z","a", "b", // المسموح استخدامها في المواقع العالمية
+            "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p","q", "r", // وعددهم 90 ولكن يوجد 3 رموز لم أضعها 
+            "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5","6", "7", // الرمزان في الأسفل تعذر استخدامهم بسبب حدوث
+            "8", "9", "(", ")", "#", "$", "&", "_", "-", "?", "!", "@", "=", "+","/", "%", // أخطاء برمجية عند وضعهم والرمز الاّخر لم أجده
+            ":", "'", "*", ".", ",", "[", "]", "{", "}", "~", "^", ">", "<", ";","|"};     // \ " الرمزان الذي تعذر استخدامهم بسبب الخطأ هم  
+
+            for (int i = 0; i < num; i++)
+            {
+                textBoxPassword.Text = textBoxPassword.Text + ranstr[rand.Next(ranstr.Length)];
+                /* 
+                 هنا عبارة عن  عداد يتم استخدامه لحساب عدد الخانات التي يتم وضعها
+                 الفكرة هنا أنه يتم أخذ الموجود في النص القديم ووضعه مع الجديد وبالنسبة لطريقة الإختيار فإنه يتم وضع المصفوفة
+                التي تحوي الرموز ويتم وضع تابع الاختيار العشوائي في مكان فهرس المصفوفة ويتم إعطاء التابع قيمة تساوي طول المصفوفة ناقص واحد
+                وهنا عندما يتم اختيار رقم عشوائي في المتغير فإنه سيكون ترتيب للعنصر الموجود في المصفوفة 
+                وهكذا يتم اختيار عناصر عشوائية من المصفوفة وانتهى ولله الحمد
+                 */
+            }
+        }
+
+        private void buttonDelet_Click(object sender, EventArgs e)
+        {
+            richTextBoxLength.Clear();
+            textBoxPassword.Clear();
+            richTextBoxLength.Focus();
+        }
+
+        private void RandomPassword_Load(object sender, EventArgs e)
+        {
+            richTextBoxLength.Select();
+        }
+
+        private void richTextBoxLength_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // هنا يتم تقييد مدخلات النص بالأرقام 
+            e.Handled = !(Char.IsDigit(e.KeyChar));
+        }
+
+        private void richTextBoxLength_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) // في حال تم ضغط زر الإدخال (Enter)
+            {
+                // يتم الغاء عمل الزر (Enter) ويتم استدعاء (buttonFond)
+                e.SuppressKeyPress = true; 
+                buttonFond.PerformClick();
+            }
+        }
+    }
+} 
+// انتهى هذا البرنامج البسيط ولله الحمد
